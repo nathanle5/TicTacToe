33ea2417d56ecee7d28fd92688c4efac6266fb96
Nathan Le <nathan.le.5@outlook.com>
2020-05-05 01:14:25 -0500

    Defined Startup LookAndFeel
    Defined the startup look and feel as Nimbus.

--- src/com/github/nathanle5/Main.java
+++ src/com/github/nathanle5/Main.java
@@ -2,6 +2,9 @@ package com.github.nathanle5;
 
 import java.awt.EventQueue;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 public class Main {
 
 	/**
@@ -11,6 +14,17 @@ public class Main {
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
+				try {
+					for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+						if ("Nimbus".equals(info.getName())) {
+							UIManager.setLookAndFeel(info.getClassName());
+							break;
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
 				try {
 					final GameFrame frame = new GameFrame();
 					frame.setLocationRelativeTo(null);
